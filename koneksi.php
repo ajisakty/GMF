0cd8b59613b4e2edd04deb354c04b5b679e52aed
ajisakty <75912325+ajisakty@users.noreply.github.com>
2023-10-16 10:43:25 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -11,6 +11,4 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
-
-echo "Koneksi berhasil!";
 ?>
--- register.php
+++ register.php
@@ -3,23 +3,60 @@
 <head>
     <title>Form Pendaftaran</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+	<?php
+	require "koneksi.php"
+	?>
 </head>
 <body>
     <h2>Form Pendaftaran</h2>
-    <form action="proses_register.php" method="post">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required><br><br>
+    <form action="#" method="post">
+        <label for="authorizationNumber">Authorization Number :</label>
+        <input type="number" id="authorizationNumber" name="authorizationNumber" required><br><br>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br><br>
+        <label for="name">Name :</label>
+        <input type="text" id="name" name="name" required><br><br>
 
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br><br>
+        <label for="IDNumber">ID Number :</label>
+        <input type="Number" id="IDNumber" name="IDNumber" required><br><br>
 
-        <label for="confirm_password">Konfirmasi Password:</label>
-        <input type="password" id="confirm_password" name="confirm_password" required><br><br>
+        <label for="birth">Date Of Birth :</label>
+        <input type="date" id="birth" name="birth" required><br><br>
+		
+		<label for="employment">Date Of Employment :</label>
+        <input type="date" id="birth" name="employment" required><br><br>
 
-        <input type="submit" value="Daftar">
+        <input type="submit" value="Register">
     </form>
+	
+	<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $authorizationNumber = $_POST["authorizationNumber"];
+    $name = $_POST["name"];
+    $idNumber = $_POST["IDNumber"];
+    $birth = $_POST["birth"];
+    $employment = $_POST["employment"];
+    
+    $formattedBirth = date('Y-m-d', strtotime($birth));
+    $formattedEmployment = date('Y-m-d', strtotime($employment));
+
+    // Query SQL untuk menyimpan data pendaftaran ke database
+    $sql = "INSERT INTO employment (authorization_number, employment_name, id_number, date_birth, date_employ) VALUES ('$authorizationNumber', '$name', '$idNumber', '$formattedBirth', '$formattedEmployment')";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "<script>alert('Register Success')</script>";
+        // Redirect pengguna ke halaman lain jika perlu
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    // Tutup koneksi ke database (jika Anda mau)
+     mysqli_close($conn);
+}
+?>
+
+
+	
+	
+	
 </body>
 </html>
